8694b89c194c0dbfd1cfeb534fe6ae5476b96dab
hungnt.jits <hungnt@jitsinnovationlabs.com>
2022-03-07 17:01:38 +0700

    add python script

--- script/start.py
+++ script/start.py
@@ -33,5 +33,5 @@ subprocess.call("sleep 5 && echo 'Starting now....'",shell=True)
 
 
 subprocess.call(f"chmod +x {_bash_dir}*",shell=True)
-getPackages()
+install_package()
 config_keystone(_ip_local_address)
