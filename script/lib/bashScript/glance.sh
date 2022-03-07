cba38053adebe6b169847694c0bba169bd0083b3
hungnt.jits <hungnt@jitsinnovationlabs.com>
2022-03-07 15:36:05 +0700

    add python script

--- script/lib/bashScript/glance.sh
+++ script/lib/bashScript/glance.sh
@@ -5,8 +5,8 @@ echo "`date`"
 controller =
 
 source /root/keystonerc
-# openstack user create --domain default --project service --password servicepassword glance
-# openstack role add --project service --user glance admin
+openstack user create --domain default --project service --password servicepassword glance
+openstack role add --project service --user glance admin
 openstack service create --name glance --description "OpenStack Image service" image
 openstack endpoint create --region RegionOne image public http://$controller:9292
 openstack endpoint create --region RegionOne image internal http://$controller:9292
