93ee9c9061416ffde7371d3ede116ce8c3a7c210
hungnt.jits <hungnt@jitsinnovationlabs.com>
2022-03-07 11:26:27 +0700

    add python script

--- script/lib/mariadb.py
+++ script/lib/mariadb.py
@@ -7,7 +7,7 @@ import re
 
 # Getting dir
 _top_dir=os.getcwd().split('\\')[-1]
-_bash_dir=_top_dir+"/bashScript/"
+_bash_dir=_top_dir+"/lib/bashScript/"
 # sys.path.insert(0,f"{_script_dir}")
 from functions import *
 print(_bash_dir)
